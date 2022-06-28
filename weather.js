0ae51ac659c5f4223ae6ff11539d1c79a0a972f4
William-Kirk <william.kirk@zealitconsultants.com>
2022-06-28 16:14:16 -0500

    javascript stuff

--- weather.js
+++ weather.js
@@ -1,48 +1,179 @@
-//FuSJTCoAfn0u2RKR9gC73brDkFNw4bHb
-//key for finding latLong
-var city = "";
-var state = "";
+//TOP
+//INSTANCE VARIABLES
+
+var zip = "75007";
+var currentCity = "";
+var currentState = "";
 var long = "";
 var lat = "";
+var weatherInformation = [];
+var currentWeather = null;
+/*
+    temp: 
+    feels: 
+    min: 
+    max: 
+    pressure: 
+    humidity: 
+    clouds: 
+    windDir: 
+    windSpeed: 
+    time: //not available for current weather
+*/
 
 
-var request = new XMLHttpRequest();
+main();
 
-//supposedly a callback hook?
-request.onreadystatechange = function () {
-    if (this.readyState === 4) {
+function main() {
+    getInformation();
+    console.log(weatherInformation);
+    
+    
+}
+
+function setMainPageInfo(){
+    setLeftMainPageInfo();
+    setRightMainPageInfo();
+}
+
+function setLeftMainPageInfo(){
+    cityState = document.getElementById('city-state');
+    timeDate = document.getElementById('time-date');
+    bigTemp = document.getElementById('big-temp');
+    cloudInfo = document.getElementById('cloud-information');
+    visualPressure = document.getElementById('pressure');
+    humidity = document.getElementById('rain-chance');
+    windSpeed = document.getElementById('wind-speed');
+
+    console.log(currentWeather);
+    cityState.innerHTML = currentCity + "," + currentState;
+    timeDate.innerHTML = "Today: " + new Date().toLocaleTimeString().substring(0,4) +(new Date().toLocaleTimeString()).substring(8);
+    bigTemp.innerHTML = Math.round(currentWeather.temp) + '°';
+    cloudInfo.innerHTML = currentWeather.clouds;
+    visualPressure.innerHTML = currentWeather.pressure + 'hpa';
+    humidity.innerHTML = currentWeather.humidity + '%';
+    windSpeed.innerHTML = Math.round(currentWeather.windSpeed) + 'km/h';
+}
+
+function setRightMainPageInfo(){
+    console.log("UGH")
+    morning = document.getElementById('morning');
+    afternoon = document.getElementById('afternoon');
+    evening = document.getElementById('evening');
+    night = document.getElementById('night');
+
+    morningCol = document.getElementById('morning-col');
+    afternoonCol = document.getElementById('afternoon-col');
+    eveningCol = document.getElementById('evening-col');
+    nightCol = document.getElementById('night-col');
+
+    morningNum = 73;
+    afternoonNum = 91;
+    eveningNum = 87;
+    nightNum = 73;
+
+    morning.innerHTML = morningNum;
+    afternoon.innerHTML = afternoonNum;
+    evening.innerHTML = eveningNum;
+    night.innerHTML = nightNum;
+
+    morningCol.style.height = morningNum + 'px';
+    afternoonCol.style.height = afternoonNum + 'px';
+    eveningCol.style.height = eveningNum + 'px';
+    nightCol.style.height = nightNum + 'px';
+    
+}
+
+async function getInformation() {
+  await getLatLong();
+  await getWeatherInfo();
+  await getCurrentWeather();
+  setMainPageInfo();
+}
+
+function getLatLong() {
+  return new Promise((resolve, reject) => {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+      if (this.readyState == 4) {
         var myLocationData = JSON.parse(request.response);
-        city = myLocationData.results[0].locations[0].adminArea5;
-        state = myLocationData.results[0].locations[0].adminArea3;
+        currentCity = myLocationData.results[0].locations[0].adminArea5;
+        currentState = myLocationData.results[0].locations[0].adminArea3;
         long = myLocationData.results[0].locations[0].latLng.lng;
         lat = myLocationData.results[0].locations[0].latLng.lat;
-        console.log("City: " + city);
-        console.log("State: " + state);
-        console.log("long: " + long);
-        console.log("lat: " + lat);
-        next();
-    }
+        console.log("Geocode Complete: " + lat + " , " + long);
+        resolve();
+      }
+    };
 
-}
-request.open('GET', 'http://www.mapquestapi.com/geocoding/v1/address?key=FuSJTCoAfn0u2RKR9gC73brDkFNw4bHb&location=75007'); //full address
-request.send();
-
-
-function next(){
-    console.log('lol')
-    request.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            var weatherData = JSON.parse(request.response);
-            console.log('Current Temperature: ' + weatherData.list[0].main.temp);
-            console.log('Temperature Low: ' + weatherData.list[0].main.temp_min);
-            console.log('Pressure: ' + weatherData.list[0].main.pressure)
-            console.log('Wind Speed: ' + weatherData.list[0].wind.speed)
-        }
-
-    }
-
-    request.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=6c3ffa046b7bec1655bb67b2e7f768fa&lat='+lat+'&long='+long+'&units=imperial'); //full address
+    var link =
+      "https://www.mapquestapi.com/geocoding/v1/address?key=FuSJTCoAfn0u2RKR9gC73brDkFNw4bHb&location=" +
+      zip;
+    request.open("GET", link);
     request.send();
+  });
 }
 
-//http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=6c3ffa046b7bec1655bb67b2e7f768fa&lat=33.005521&long=-96.894142&units=imperial
+function getWeatherInfo(){
+    return new Promise((resolve, reject) =>{
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function(){
+            if(this.readyState == 4){
+                myWeatherData=JSON.parse(request.response);
+                for(let i = 0; i < 40; i++){
+                    let main = myWeatherData.list[i].main;
+                    let stuff = {
+                        temp: main.temp,
+                        feels: main.feels_like,
+                        min: main.temp_min,
+                        max: main.temp_max,
+                        pressure: main.pressure,
+                        humidity: main.humidity,
+                        clouds: myWeatherData.list[i].weather[0].description,
+                        windDir: myWeatherData.list[i].wind.deg,
+                        windSpeed: myWeatherData.list[i].wind.speed,
+                        time: myWeatherData.list[i].dt_txt
+                    }
+                    weatherInformation.push(stuff);
+                }
+                resolve();
+            }
+        }
+        var link = '';
+        if(lat != "")
+            link = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&appid=f9365e105ac4d399424f1b7351a3c941&units=imperial";
+        console.log(link);
+        request.open("GET", link);
+        request.send();
+    });
+}
+
+function getCurrentWeather(){
+    return new Promise((resolve, reject) =>{
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function(){
+            if(this.readyState == 4){
+                myWeatherData=JSON.parse(request.response);
+                var main = myWeatherData.main;
+                currentWeather = {
+                    temp: main.temp,
+                    feels: main.feels_like,
+                    min: main.temp_min,
+                    max: main.temp_max,
+                    pressure: main.pressure,
+                    humidity: main.humidity,
+                    clouds: myWeatherData.weather[0].description,
+                    windDir: myWeatherData.wind.deg,
+                    windSpeed: myWeatherData.wind.speed
+                }
+                resolve();
+            }
+        }
+        var link = '';
+        if(lat != "")
+            link = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=f9365e105ac4d399424f1b7351a3c941&units=imperial";
+        console.log(link);
+        request.open("GET", link);
+        request.send();
+    });
+}
