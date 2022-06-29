fc41fc563e13ef49122313ddbe63e09f58fb498c
William-Kirk <william.kirk@zealitconsultants.com>
2022-06-29 13:52:18 -0500

    OH MY GOD I FINISHED THE WEATHER DASHBOARD FINALLY

--- weather.js
+++ weather.js
@@ -14,14 +14,27 @@ var eveningWeather = 0;
 var nightWeather = 0;
 var rainChance = 0;
 var currentUV = 0;
+
+var nextNowImg
 var nextHourOne;
+var nextHourOneImg;
 var nextHourTwo;
+var nextHourTwoImg;
 var nextHourThree;
+var nextHourThreeImg;
+
 var futureDay0;
+var futureDay0Clouds;
 var futureDay1;
+var futureDay1Clouds;
 var futureDay2;
+var futureDay2Clouds;
 var futureDay3;
-var futureDay4;
+var futureDay3Clouds;
+
+var partlyCloudy='https://cdn.weatherapi.com/weather/64x64/day/116.png';
+var sunny = 'https://cdn.weatherapi.com/weather/64x64/day/113.png';
+var rainy = 'https://cdn.weatherapi.com/weather/64x64/day/302.png';
 /*
     temp: 
     feels: 
@@ -52,7 +65,7 @@ function start(){
                 console.log(weatherInformation);
             }
     });
-    document.getElementById('zip-code').addEventListener('input', activate);
+    document.getElementById('zip-search-bar').addEventListener('click', activate);
 }
 
 function activate() {
@@ -75,22 +88,67 @@ function setMainPageInfo() {
 
 function setBottomSidePageInfo(){
     var dt = new Date();
-    document.getElementById('day-1').innerHTML = futureDay0 + '°';
-    document.getElementById('day-2').innerHTML = futureDay1 + '°';
+    document.getElementById('day-1').innerHTML = Math.round(futureDay0) + '°';
+    document.getElementById('day-2').innerHTML = Math.round(futureDay1) + '°';
+    document.getElementById('day-1-img').src = futureDay0Clouds;
+    document.getElementById('day-2-img').src = futureDay1Clouds;
 
-    //TODO REMEMBER
-    document.getElementById('day-3').innerHTML = futureDay0 + '°';
-    document.getElementById('day-4').innerHTML = futureDay1 + '°';
+    document.getElementById('day-3').innerHTML = Math.round(futureDay2) + '°';
+    document.getElementById('day-4').innerHTML = Math.round(futureDay3) + '°';
 
-    document.getElementById('')
-
+    document.getElementById('day-2-week').innerHTML = quickWeek(1);
+    document.getElementById('day-3-week').innerHTML = quickWeek(2);
+    document.getElementById('day-4-week').innerHTML = quickWeek(3);
 
     document.getElementById('day-1-date').innerHTML = quickDate(1);
     document.getElementById('day-2-date').innerHTML = quickDate(2);
     document.getElementById('day-3-date').innerHTML = quickDate(3);
     document.getElementById('day-4-date').innerHTML = quickDate(4);
 
+    day2Img = sunny;
+    day3Img = sunny;
+
+
+    if(futureDay3Clouds == "Clouds")
+        day3Img = partlyCloudy;
+    else if(futureDay3Clouds == 'Clear')
+        console.log('its clear');
+    else if(futureDay3Clouds == 'Rain')
+        day3Img = rainy;
+
+    if(futureDay2Clouds == "Clouds")
+        day2Img = partlyCloudy;
+    else if(futureDay2Clouds == 'Clear')
+        day2Img = sunny;
+    else if(futureDay2Clouds == 'Rain')
+        console.log('its rainy');
+        day2Img = rainy;
+
+    document.getElementById('day-3-img').src = day2Img;
+    document.getElementById('day-4-img').src = day3Img;
+
+
 }
+function quickWeek(addDays){
+    currentDay = new Date().getDay();
+    currentDay += addDays;    
+
+    if(currentDay == 0)
+        return 'Sunday';
+    if(currentDay == 1)
+        return 'Monday';
+    if(currentDay == 2)
+        return 'Tuesday';
+    if(currentDay == 3)
+        return 'Wednesday';
+    if(currentDay == 4)
+        return 'Thursday';
+    if(currentDay == 5)
+        return 'Friday'
+    if(currentDay == 6)
+        return 'Saturday';
+}
+
 
 function quickDate(addDays){
     var dt = new Date();
@@ -184,6 +242,11 @@ function setTopSidePageInfo(){
     hourTwoContent.innerHTML = Math.round(nextHourTwo) + '°';
     hourThreeContent.innerHTML = Math.round(nextHourThree) + '°';
 
+    nowImg.src = 'https:' + nextNowImg;
+    hourOneImg.src = 'https:' + nextHourOneImg;
+    hourTwoImg.src = 'https:' + nextHourTwoImg;
+    hourThreeImg.src = 'https:' + nextHourThreeImg;
+
     //nowImg =
     //hourOneImg
 }
@@ -198,6 +261,7 @@ function quickTime(addNum){
 
 
 async function getInformation() {
+    document.getElementById('loading-img').style.display = 'block';
     await getLatLong();
     await getWeatherInfo();
     await getCurrentWeather();
@@ -205,6 +269,7 @@ async function getInformation() {
     await currentWeatherUV();
     await getWeatherForecast();
     await cmonFuture();
+    document.getElementById('loading-img').style.display = 'none';
     setMainPageInfo();
 }
 
@@ -251,6 +316,12 @@ function getWeatherInfo() {
                         windSpeed: myWeatherData.list[i].wind.speed,
                         time: myWeatherData.list[i].dt_txt
                     }
+                    futureDay2Clouds = myWeatherData.list[24].weather[0].main;
+                    futureDay3Clouds = myWeatherData.list[32].weather[0].main;
+                    
+
+                    futureDay2 = myWeatherData.list[24].main.temp;
+                    futureDay3 = myWeatherData.list[32].main.temp;
                     weatherInformation.push(stuff);
                 }
                 resolve();
@@ -259,7 +330,7 @@ function getWeatherInfo() {
         var link = '';
         if (lat != "")
             link = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&appid=f9365e105ac4d399424f1b7351a3c941&units=imperial";
-        console.log(link);
+            console.log('lol: '+ link);
         request.open("GET", link);
         request.send();
     });
@@ -331,19 +402,25 @@ function getWeatherForecast() {
                 nightWeather = Math.round(myData.forecast.forecastday[0].hour[23].temp_f);
                 num =((myData.current.last_updated).substring(11,13));
                 rainChance = Math.round(myData.forecast.forecastday[0].hour[Number(num)].chance_of_rain)
+                num = num = Number((myData.current.last_updated).substring(11,13));
+                nextNowImg = myData.forecast.forecastday[0].hour[num].condition.icon;
                 num = Number((myData.current.last_updated).substring(11,13)) + 1;
                 if(num >23)
                     num-=24;
                 nextHourOne = myData.forecast.forecastday[0].hour[num].temp_f;
+                nextHourOneImg = myData.forecast.forecastday[0].hour[num].condition.icon;
                 num = Number((myData.current.last_updated).substring(11,13)) + 2;
                 if(num >23)
                     num-=24;
                 nextHourTwo = myData.forecast.forecastday[0].hour[num].temp_f;
+                nextHourTwoImg = myData.forecast.forecastday[0].hour[num].condition.icon;
                 num = Number((myData.current.last_updated).substring(11,13)) + 3;
                 if(num >23)
                     num-=24;
                 nextHourThree = myData.forecast.forecastday[0].hour[num].temp_f;
+                nextHourThreeImg = myData.forecast.forecastday[0].hour[num].condition.icon;
                 resolve();
+                console.log(nextHourOneImg + '\n' + nextHourTwoImg + '\n' + nextHourThreeImg + num);
             }
         }
         var link = '';
@@ -390,7 +467,9 @@ function cmonFuture() {
             if (this.readyState == 4) {
                 var myData = JSON.parse(request.response);
                 futureDay0 = myData.forecast.forecastday[1].day.avgtemp_f;
+                futureDay0Clouds = 'https:' + myData.forecast.forecastday[1].day.condition.icon;
                 futureDay1 = myData.forecast.forecastday[2].day.avgtemp_f;
+                futureDay1Clouds = 'https:' + myData.forecast.forecastday[2].day.condition.icon;
 
                 resolve();
             }
@@ -403,6 +482,4 @@ function cmonFuture() {
         request.send();
     });
 }
-
-
 
